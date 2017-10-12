e81218e6dd9302e46484d818f964784bea89e498
adnredbot <32553646+adnredbot@users.noreply.github.com>
2017-10-12 18:43:52 +1100

    Update and rename latest.py to batoto.py

--- latest/batoto.py
+++ latest/batoto.py
@@ -3,14 +3,14 @@ from discord.ext import commands
 from bs4 import BeautifulSoup
 import aiohttp
 
-class latest:
+class batoto:
     """A custom cog that will grab the url of the latest upload"""
 
     def __init__(self, bot):
         self.bot = bot
 
     @commands.command(pass_context=True)
-    async def latest(self, ctx):
+    async def batoto(self, ctx):
         """Get the latest download release"""
 
         #BeautifulSoup
@@ -27,4 +27,4 @@ class latest:
             return await self.bot.send_message(ctx.message.author, "Command was unsuccessful due to error.")
         
 def setup(bot):
-    bot.add_cog(latest(bot))
+    bot.add_cog(batoto(bot))
